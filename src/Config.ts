dc85b6b0b5bb0f5d0910f76c224ee2ceb342c520
Per Kristian Kummermo <per.kristian@telenordigital.com>
2018-05-08 14:45:18 +0200

    refactor(config): export config for mapper constructors

--- src/Config.ts
+++ src/Config.ts
@@ -1,3 +1,10 @@
+export { IBase64Config } from "./mappers/Base64";
+export { IChunkConfig } from "./mappers/Chunk";
+export { IFromJSONConfig } from "./mappers/FromJSON";
+export { IHexToFloatConfig } from "./mappers/HexToFloat";
+export { IHexToIntConfig } from "./mappers/HexToInt";
+export { IOffsetConfig } from "./mappers/Offset";
+
 export enum Endianness {
   /**
    * Config parameter for defining little endianness
