152867b0908224edf0fb098e11c93d3eee620e47
Per Kristian Kummermo <per.kristian@telenordigital.com>
2018-05-13 18:51:04 +0200

    feat(factory): add DataMapperChain factory in main
    Fixes #6

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -12,4 +12,25 @@ describe("Main export", () => {
   it("should have mappers exported", () => {
     expect(Main.Mappers).toBeDefined();
   });
+
+  describe("DataMapperChain factory", () => {
+    it("should have a create export", () => {
+      expect(Main.create).toBeDefined();
+    });
+
+    it("should correctly create a default DataMapperChain upon create", () => {
+      const mapper = Main.create();
+
+      expect(mapper instanceof Main.DataMapperChain).toBe(true);
+    });
+
+    it("should correctly take a configuration object for DataMapperChain upon create", () => {
+      const mapper = Main.create({
+        name: "My mapper",
+      });
+
+      expect(mapper instanceof Main.DataMapperChain).toBe(true);
+      expect(mapper.name).toBe("My mapper");
+    });
+  });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
+import { DataMapperChain, IDataMapperChainConfig } from "./DataMapperChain";
+
 export * from "./DataMapperChain";
 export * from "./Config";
 export * from "./Mappers";
 export * from "./Typings";
+
+export const create = (newDataMapperChainParams: IDataMapperChainConfig = {}): DataMapperChain => {
+  return new DataMapperChain(newDataMapperChainParams);
+};
