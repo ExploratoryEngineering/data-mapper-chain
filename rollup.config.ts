f70c2734d28994075da4693f7cc6cdcdef3a3bfc
Per Kristian Kummermo <per.kristian@telenordigital.com>
2018-11-26 13:23:27 +0100

    refactor(rollup-config): use terser intead of uglifyjs

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,23 +2,21 @@ import commonjs from "rollup-plugin-commonjs";
 import json from "rollup-plugin-json";
 import resolve from "rollup-plugin-node-resolve";
 import sourceMaps from "rollup-plugin-sourcemaps";
+import { terser } from "rollup-plugin-terser";
 import typescript from "rollup-plugin-typescript2";
-import uglify from "rollup-plugin-uglify";
 
 // tslint:disable-next-line:no-var-requires
 const pkg = require("./package.json");
 
 const libraryName = "dmc";
 
-export default {
-    entry: `src/main.ts`,
-    output: [
-        { file: pkg.main, name: libraryName, format: "umd" },
-        { file: pkg.module, format: "es" },
-    ],
-    sourcemap: true,
+export default [{
+    input: `src/main.ts`,
+    output: {
+        file: pkg.module,
+        format: "es",
+    },
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    external: [],
     watch: {
         include: "src/**",
     },
@@ -37,6 +35,34 @@ export default {
         // Resolve source maps to the original source
         sourceMaps(),
 
-        uglify(),
+        terser(),
     ],
-};
+}, {
+    input: `src/main.ts`,
+    output: {
+        file: pkg.main,
+        name: libraryName,
+        format: "umd",
+    },
+    // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    watch: {
+        include: "src/**",
+    },
+    plugins: [
+        // Allow json resolution
+        json(),
+        // Compile TypeScript files
+        typescript({ useTsconfigDeclarationDir: true }),
+        // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+        commonjs(),
+        // Allow node_modules resolution, so you can use 'external' to control
+        // which external modules to include in the bundle
+        // https://github.com/rollup/rollup-plugin-node-resolve#usage
+        resolve(),
+
+        // Resolve source maps to the original source
+        sourceMaps(),
+
+        terser(),
+    ],
+}];
