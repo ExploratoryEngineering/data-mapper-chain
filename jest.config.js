9f702fcf9e05751f5ffef2dd743fe826d60cb7be
Per Kristian Kummermo <per.kristian@telenordigital.com>
2019-10-23 15:43:14 +0200

    chore(versions): bump versions with security issues

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  preset: "ts-jest",
+  // testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      tsConfig: "tsconfig.jest.json",
+    },
+  },
+  collectCoverage: true,
+  coverageDirectory: "./coverage/",
+  coverageReporters: ["json", "lcov", "text", "html"],
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
+};
